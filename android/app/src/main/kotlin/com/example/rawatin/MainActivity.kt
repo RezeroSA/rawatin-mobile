a3d06232f6f422ad89c1852a50a9dff432dc791c
Sandy Alferro <sandyalferro@Sandys-MacBook-Pro.local>
2023-10-15 21:47:52 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/rawatin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rawatin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class RawatinColorTheme {
+  static const Color mainOrange = Color(0xFFFD841F);
+  static const Color secondaryBlack = Color(0xFF000000);
+  static const Color secondaryOrange = Color(0xFFFFF1E5);
+  static const Color secondaryRed = Color(0xFFFF0707);
+  static const Color secondaryGrey = Color(0xFF4B4B4B);
+}
